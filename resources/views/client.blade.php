b0bde17611b54f8db19520ffbe8b59af8e8d065b
FGabriel0 <gabrielfabio3g@gmail.com>
2022-01-24 21:45:19 -0300

    Desenvolvimento do Html, inicialização do footer,formulário do cliente também a criação da pasta layouts com a main e das rotas

---
+++ resources/views/client.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.main')
+
+@section('title', 'Agendamento')
+    
+@section('content')
+        <h1>AGENDAMENTO</h1>
+
+        <form>
+            <div class="form-group">
+              <label for="exampleFormControlInput1">Nome:</label>
+              <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Digite seu nome">
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlSelect1">Número de contato:</label>
+              <input type="number" class="form-control" id="number" placeholder="Digite seu número">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlSelect1">Escolha uma Data:</label>
+                <input type="date" class="form-control" id="date" placeholder="Escolha uma data">
+              </div>
+            <div class="form-group">
+              <label for="exampleFormControlSelect2">Cortes:</label>
+              <select name="cortes" id="cortes" class="form-control">
+                  <option>Social</option>
+                  <option>Degrade</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1">Sugestão:</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Alguma sugestão para o corte?"></textarea>
+            </div>
+          </form>
+          <button type="submit" class="btn btn-primary mb-2">Enviar</button>
+
+ @include('layouts.footer')
+
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="bg-dark text-center text-white">
+    <div class="container p-4">
+      <section class="mb-4">
+
+        <a class="btn btn-outline-light btn-floating m-1 btn-footer-hover" role="button" target="_blank" href="#"><i class="fab fa-instagram"></i></a>
+        <a class="btn btn-outline-light btn-floating m-1 btn-footer-hover" role="button" target="_blank" href="#"><i class="fab fa-whatsapp"></i></a>
+
+        <p>JP Barbearia &copy; 2022</p>
+      </section>
+    </div>
