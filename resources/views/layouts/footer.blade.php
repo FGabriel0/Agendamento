a9f48fa242ee4ccaad5d7b5373ed2d224cab3077
FGabriel0 <gabrielfabio3g@gmail.com>
2022-02-05 10:33:54 -0300

    Novo design do footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,12 @@
 <footer class="bg-dark text-center text-white">
     <div class="container p-4">
       <section class="mb-4">
-
-        <a class="btn btn-outline-light btn-floating m-1 btn-footer-hover" role="button" target="_blank" href="https://www.instagram.com/jpbarbeariamoura/">
-          <i class="fab fa-instagram"></i>
-        </a>
-
-        <a class="btn btn-outline-light btn-floating m-1 btn-footer-hover" role="button" target="_blank" href="#">
-          <i class="fab fa-whatsapp"></i>
-        </a>
-
         <p>JP Barbearia &copy; 2022</p>
+
+        <div class="social-midia">
+          <p><i class="fab fa-instagram"></i>@jpbarbeariamoura</p>
+          <p><i class="fab fa-whatsapp"></i>(+55) 85 98201-5615</p>
+        </div>
+        
       </section>
     </div>
